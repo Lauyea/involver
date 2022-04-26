6ebfb4a9ed37f5f37af7c0e6a16bf06c3ef2204a
Lauyea <lauyea.shea@gmail.com>
2022-04-26 11:08:11 +0800

    新增Anti Xss，待測試

---
+++ Involver/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Ganss.XSS;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Involver.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string AntiXssRaw(this IHtmlHelper helper, string html) 
+        {
+            var sanitizer = new HtmlSanitizer();
+
+            var sanitized = sanitizer.Sanitize(html);
+
+            return sanitized;
+        }
+    }
+}
