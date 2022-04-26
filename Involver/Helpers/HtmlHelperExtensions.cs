8b3e2912f5e67c8e36f727444aa9710da7bd4298
Lauyea <lauyea.shea@gmail.com>
2022-04-26 11:21:14 +0800

    patch

--- Involver/Helpers/HtmlHelperExtensions.cs
+++ Involver/Helpers/HtmlHelperExtensions.cs
@@ -1,17 +1,27 @@
 ﻿using Ganss.XSS;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Involver.Helpers
 {
+    /// <summary>
+    /// HtmlHelper 的擴充
+    /// </summary>
     public static class HtmlHelperExtensions
     {
-        public static string AntiXssRaw(this IHtmlHelper helper, string html) 
+        /// <summary>
+        /// 內嵌 HTML 時的 XSS 防護
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static IHtmlContent AntiXssRaw(this IHtmlHelper helper, string html) 
         {
             var sanitizer = new HtmlSanitizer();
 
             var sanitized = sanitizer.Sanitize(html);
 
-            return sanitized;
+            return helper.Raw(sanitized);
         }
     }
 }
