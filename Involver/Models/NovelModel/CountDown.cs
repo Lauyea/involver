b02b550856c13014e188947c7e6ed0c93cd08719
Lauyea <51250275+Lauyea@users.noreply.github.com>
2022-06-14 11:51:46 +0800

    Partial voting adjustment (#26)
    * Partial votings
    
    * Adjust votings layout

---
+++ Involver/Models/NovelModel/CountDown.cs
@@ -0,0 +1,15 @@
+﻿namespace Involver.Models.NovelModel
+{
+    public class CountDown
+    {
+        /// <summary>
+        /// 倒數時間
+        /// </summary>
+        public DateTime? countDownTime { get; set; }
+
+        /// <summary>
+        /// HTML id
+        /// </summary>
+        public string id { get; set; }
+    }
+}
