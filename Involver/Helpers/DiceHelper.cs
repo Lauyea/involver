3b391f5e13c6f43cf078024a990f54f4587e137e
Lauyea <51250275+Lauyea@users.noreply.github.com>
2022-08-08 10:47:20 +0800

    Add DiceHelper and UnitTest (#65)

---
+++ Involver/Helpers/DiceHelper.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace Involver.Helpers
+{
+    public static class DiceHelper
+    {
+        public static bool ReplaceRollDiceString(ref string strToDice)
+        {
+            Random random = new();
+            int start = 0;
+            int at = 0;
+            int end = strToDice.Length;
+            int count;
+            int diceValue;
+            bool hasChange = false;
+
+            while ((start <= end) && (at > -1))
+            {
+                end = strToDice.Length;
+                diceValue = 0;
+                count = end - start;
+                at = strToDice.IndexOf("Dice", start, count);
+                //-1 等於沒找到
+                if (at == -1) break;
+                //Dice05D10，D 在index = 6
+                if (strToDice[at + 6] == 'D')
+                {
+                    hasChange = true;
+                    int rollTimes = int.Parse(strToDice[at + 4].ToString() + strToDice[at + 5].ToString());
+                    int diceSides = int.Parse(strToDice[at + 7].ToString() + strToDice[at + 8].ToString());
+
+                    int iterativeTimes = rollTimes;
+
+                    while (iterativeTimes > 0)
+                    {
+                        diceValue += random.Next(1, diceSides + 1);
+                        iterativeTimes--;
+                    }
+
+                    string strToBeChanged;
+                    string changingStr;
+                    if (rollTimes < 10 && diceSides < 10)
+                    {
+                        strToBeChanged = "Dice0" + rollTimes + "D0" + diceSides;
+                        changingStr = rollTimes + "D0" + diceSides + ": " + diceValue;
+                    }
+                    else if (rollTimes < 10)
+                    {
+                        strToBeChanged = "Dice0" + rollTimes + "D" + diceSides;
+                        changingStr = rollTimes + "D" + diceSides + ": " + diceValue;
+                    }
+                    else if (diceSides < 10)
+                    {
+                        strToBeChanged = "Dice" + rollTimes + "D0" + diceSides;
+                        changingStr = rollTimes + "D0" + diceSides + ": " + diceValue;
+                    }
+                    else
+                    {
+                        strToBeChanged = "Dice" + rollTimes + "D" + diceSides;
+                        changingStr = rollTimes + "D" + diceSides + ": " + diceValue;
+                    }
+                    StringBuilder stringBuilder = new StringBuilder(strToDice);
+                    stringBuilder.Replace(strToBeChanged, changingStr, at, strToBeChanged.Length);
+                    strToDice = stringBuilder.ToString();
+                }
+                //因為StringBuilder 可以分段改字串了，而且Roll也不見了，就不需要改start 的位置了(X)
+                //start變動，不用每次都從頭算，剩下需要計算的位數會變少，增加效率
+
+                // Dice has 4 digits
+                start = at + 5;
+            }
+
+            return hasChange;
+        }
+    }
+}
---
+++ InvolverTest/Helpers/DiceHelperTests.cs
@@ -0,0 +1,87 @@
+﻿using Involver.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace InvolverTest.Helpers
+{
+    [TestFixture]
+    public class DiceHelperTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void ReplaceRollDiceString_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string strToDice = "Dice05D10";
+
+            // Act
+            var result = DiceHelper.ReplaceRollDiceString(
+                ref strToDice);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual("5D10: ", strToDice[..6]);
+        }
+
+        [Test]
+        public void ReplaceRollDiceString_StateUnderTest_ExpectedBehavior_LongString()
+        {
+            // Arrange
+            string strToDice = "<p>Dice05D10</p><p>Dice05D10</p><p>Dice05D10</p><p>Dice05D10</p><p>Dice05D10</p>";
+
+            // Act
+            var result = DiceHelper.ReplaceRollDiceString(
+                ref strToDice);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual("<p>5D10: ", strToDice[..9]);
+        }
+
+        [Test]
+        public void ReplaceRollDiceString_StateUnderTest_ExpectedBehavior_TestEven()
+        {
+            // Arrange
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+
+            // Act
+            for(int i = 0; i < 100; i++)
+            {
+                string strToDice = "Dice01D03";// EX: "1D3: 2"
+
+                var result = DiceHelper.ReplaceRollDiceString(
+                    ref strToDice);
+
+                var lastNumber = strToDice[6];
+
+                if (lastNumber == '1')
+                {
+                    count1++;
+                }
+                else if(lastNumber == '2')
+                {
+                    count2++;
+                }
+                else
+                {
+                    count3++;
+                }
+            }
+
+            // Assert
+            Assert.IsTrue(count1 > 0);
+            Assert.IsTrue(count2 > 0);
+            Assert.IsTrue(count3 > 0);
+        }
+    }
+}
