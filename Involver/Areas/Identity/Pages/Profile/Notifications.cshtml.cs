043af096911f0e05573a6b433e6c00422418410b
Lauyea <lauyea.shea@gmail.com>
2022-08-27 15:38:40 +0800

    adjust profile layout

--- Involver/Areas/Identity/Pages/Profile/Notifications.cshtml.cs
+++ Involver/Areas/Identity/Pages/Profile/Notifications.cshtml.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace Involver.Areas.Identity.Pages.Profile
 {
