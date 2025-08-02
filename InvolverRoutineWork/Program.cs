39ee53af3f7889affaa146189ead76e1acb0842e
Lauyea <51250275+Lauyea@users.noreply.github.com>
2025-08-02 10:29:55 +0800

    Debug Funcion沒有正確註冊DbContext的問題 (#106)
    .Net 8 要在Program註冊DbContext

--- InvolverRoutineWork/Program.cs
+++ InvolverRoutineWork/Program.cs
@@ -1,7 +1,17 @@
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        var connectionString = Environment.GetEnvironmentVariable("sqldb_connection");
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer(connectionString));
+    })
     .Build();
 
 host.Run();
