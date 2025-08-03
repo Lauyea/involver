8a361b89b860960429ca671dcbffe819db28d363
Lauyea <lauyea.shea@gmail.com>
2025-08-03 11:52:37 +0800

    將Dark Mode Cookie改存在Client端瀏覽器上

--- Involver/Controllers/DarkModeController.cs
+++ Involver/Controllers/DarkModeController.cs
@@ -10,22 +10,42 @@ namespace Involver.Controllers
     {
         string _sessionKey = "_DarkMode";
 
+        /// <summary>
+        /// Server-side Session
+        /// </summary>
+        //[HttpGet]
+        //public void Set()
+        //{
+        //    string sessionValue = HttpContext.Session.GetString(_sessionKey);
+        //    if (string.IsNullOrEmpty(sessionValue))
+        //    {
+        //        HttpContext.Session.SetString(_sessionKey, "On");
+        //    }
+        //    else if (sessionValue == "On")
+        //    {
+        //        HttpContext.Session.SetString(_sessionKey, "Off");
+        //    }
+        //    else
+        //    {
+        //        HttpContext.Session.SetString(_sessionKey, "On");
+        //    }
+        //}
+
+        /// <summary>
+        /// Client-side Cookie
+        /// </summary>
         [HttpGet]
         public void Set()
         {
-            string sessionValue = HttpContext.Session.GetString(_sessionKey);
-            if (string.IsNullOrEmpty(sessionValue))
+            string cookieValue = Request.Cookies[_sessionKey];
+            string newValue = (cookieValue == "On") ? "Off" : "On";
+            Response.Cookies.Append(_sessionKey, newValue, new CookieOptions
             {
-                HttpContext.Session.SetString(_sessionKey, "On");
-            }
-            else if (sessionValue == "On")
-            {
-                HttpContext.Session.SetString(_sessionKey, "Off");
-            }
-            else
-            {
-                HttpContext.Session.SetString(_sessionKey, "On");
-            }
+                Expires = DateTimeOffset.UtcNow.AddYears(1), // 可依需求調整
+                HttpOnly = false, // 若要讓 JS 也能存取，設為 false
+                Secure = true,    // 若只允許 HTTPS，設為 true
+                SameSite = SameSiteMode.Lax
+            });
         }
     }
 }
