bbe7a66e893f8422210fd0dc54de2db3353c695f
LAUYEAPC\lauyea <lauyea.shea@gmail.com>
2025-08-22 14:50:35 +0800

    Remove unnecessary DB project

--- Database/dbo/Tables/NovelTags.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[NovelTags] (
-    [TagId] INT           IDENTITY (1, 1) NOT NULL,
-    [Name]  NVARCHAR (15) NOT NULL,
-    CONSTRAINT [PK_NovelTags] PRIMARY KEY CLUSTERED ([TagId] ASC)
-);
-
--- Database/dbo/Tables/ProfitSharing.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ProfitSharing] (
-    [ProfitSharingID] INT            IDENTITY (1, 1) NOT NULL,
-    [InvolverID]      NVARCHAR (MAX) NULL,
-    [CreditCard]      NVARCHAR (MAX) NULL,
-    [SharingValue]    INT            NOT NULL,
-    [SharingDone]     BIT            NOT NULL,
-    CONSTRAINT [PK_ProfitSharing] PRIMARY KEY CLUSTERED ([ProfitSharingID] ASC)
-);
-
