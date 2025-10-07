0739e8a92fc8c1e78c4c0b68a344b7af69013814
Lauyea <51250275+Lauyea@users.noreply.github.com>
2025-10-07 09:04:42 +0800

    標籤雲允許匿名用戶 (#153)

--- Involver/Pages/Articles/Tags.cshtml.cs
+++ Involver/Pages/Articles/Tags.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Involver.Pages.Articles
 {
+    [AllowAnonymous]
     public class TagsModel : PageModel
     {
         public void OnGet()
