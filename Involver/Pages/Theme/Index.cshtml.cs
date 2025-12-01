6142a6557ef5c6ac2e5c441c1182035b03284d35
LAUYEAPC\lauyea <lauyea.shea@gmail.com>
2025-12-01 11:24:49 +0800

    寫一個專門用來檢查CSS排版的頁面

---
+++ Involver/Pages/Theme/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Involver.Pages.Theme
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
